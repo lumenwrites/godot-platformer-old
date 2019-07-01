e7a636af5b85d123e0a0a7a6401e17549548853b
Ray Alez <raymestalez@gmail.com>
2019-06-30 23:44:11 -0700

    Update

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,13 +1,13 @@
 extends KinematicBody2D
 
-const MAX_SPEED = 500
-const ACCELERATION = 2500
-const DECELERATION = 2500
-const GRAVITY = 2500
+const MAX_SPEED = 1500
+const ACCELERATION = 5000
+const DECELERATION = 5000
+const GRAVITY = 5000
 const UP = Vector2(0,-1)
-const JUMP_SPEED = 400
+const JUMP_SPEED = 500
 const JUMP_BOOST = 2 # Multiplier for jump pads
-
+const DASH_SPEED = 1400
 var speed = 0
 var velocity = Vector2()
 export var world_limit = 3000
@@ -27,36 +27,40 @@ func update_velocity(delta):
 	fall(delta)	
 	run(delta)
 	jump(delta)
+	decelerate(delta)
+	dash(delta)
 	move_and_slide(velocity, UP)
 	
 	
 
 
 
-func run(delta):	
-	# Cap velocity
-	if velocity.x > MAX_SPEED:
-		velocity.x = MAX_SPEED
-	if velocity.x < -MAX_SPEED:
-		velocity.x = -MAX_SPEED		
-	# Accelerate
-	if Input.is_action_pressed("ui_right"):
+func run(delta):		
+
+	# Accelerate (capped at max speed)
+	if velocity.x < MAX_SPEED and Input.is_action_pressed("ui_right"): 
 		velocity.x += ACCELERATION*delta
-	if Input.is_action_pressed("ui_left"):
+	if velocity.x > -MAX_SPEED and Input.is_action_pressed("ui_left"): 
 		velocity.x -= ACCELERATION*delta
+
+	Global.HUD.update_HUD("velocity.x " + str(velocity.y))
+
+func decelerate(delta):
+	# friction simply modifies velocity in the opposite direction
+	velocity.x = velocity.x * 0.9
 	
 	# Decelerate, simply subtract deceleration from velocity until it reaches 0
 	# I decelerate as long as I'm not pressing accelerate buttton
 	# It's a separate if, because if I want to move in the opposite direction, deceleration will be applied on top of acceleration
-	if velocity.x > 0 and not Input.is_action_pressed("ui_right"):
-		velocity.x -= DECELERATION*delta
-		if velocity.x < 0: velocity.x = 0 # If I have subtracted too much and it's below 0 now, set it to 0
-	elif velocity.x < 0 and not Input.is_action_pressed("ui_left"):
-		velocity.x += DECELERATION*delta
-		if velocity.x > 0: velocity.x = 0
-		
-	Global.HUD.update_HUD("velocity.x " + str(velocity.y))
+#	if velocity.x > 0 and not Input.is_action_pressed("ui_right"):
+#		velocity.x -= DECELERATION*delta
+#		if velocity.x < 0: velocity.x = 0 # If I have subtracted too much and it's below 0 now, set it to 0
+#	elif velocity.x < 0 and not Input.is_action_pressed("ui_left"):
+#		velocity.x += DECELERATION*delta
+#		if velocity.x > 0: velocity.x = 0
 
+	
+	
 const JUMP_TIME = 0.5
 var jumpTimer = JUMP_TIME
 var isJumping = false
@@ -65,7 +69,7 @@ func jump(delta):
 	Global.HUD.update_HUD("velocity.y " + str(velocity.y))
 	# Start jumping. Add initial velocity, and start jump timer
 	if is_on_floor() and Input.is_action_pressed("ui_jump"):
-		velocity.y = -JUMP_SPEED
+		velocity.y = -JUMP_SPEED*2
 		isJumping = true
 		jumpTimer = JUMP_TIME
 	
@@ -78,6 +82,26 @@ func jump(delta):
 		if Input.is_action_just_released("ui_jump") or jumpTimer < 0:
 			isJumping = false
 
+var dashDirection = Vector2()
+var canDash = true
+func dash(delta):
+	dashDirection.x = 0
+	dashDirection.y = 0
+	if Input.is_action_pressed("ui_right"): dashDirection.x = 1
+	if Input.is_action_pressed("ui_left"): dashDirection.x = -1
+	if Input.is_action_pressed("ui_up"): dashDirection.y = -1
+	if Input.is_action_pressed("ui_down"): dashDirection.y = 1
+	dashDirection = dashDirection.normalized()
+	if Input.is_action_pressed("ui_dash") and canDash: 	
+		velocity.x += dashDirection.x * DASH_SPEED
+		velocity.y += dashDirection.y * DASH_SPEED * 2 # dash up feels smaller for some reason
+		canDash = false
+		print("Dash")
+		print(dashDirection.x)
+		print(dashDirection.y)
+		print(canDash)
+	
+
 func boost():
 	velocity.y = -1500
 
@@ -85,8 +109,9 @@ func fall(delta):
 	if is_on_ceiling():
 		velocity.y = 0
 	if is_on_floor():
-		pass
-		#velocity.y = 0
+		# recharge dash
+		canDash = true
+		velocity.y = 0
 	else:
 		# move_and_slide already multiplies thingamabob by delta
 		# this multiplies by delta second time, because that's how acceleration works
